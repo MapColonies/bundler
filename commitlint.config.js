78e54491d4f6a91a6a1e9bdf7a57826d887a027d
melancholiai <samit2310@gmail.com>
2023-02-26 15:09:34 +0200

    feat(configurations): set up husky with lint and commitlint hooks

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-enum': async ctx =>
-      [2, 'always', [...(await getPackages(ctx)), "deps", "configurations"]]
+      [2, 'always', [...(await getPackages(ctx)), "deps", "configurations"]],
+      "scope-empty": [2, "never"],
   }
 };
