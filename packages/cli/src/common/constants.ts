4e5f07863f712423c803533dcc9a20e10cc357d5
melancholiai <samit2310@gmail.com>
2023-03-29 11:56:06 +0300

    configurations: updated for publishing

--- packages/cli/src/common/constants.ts
+++ packages/cli/src/common/constants.ts
@@ -1,8 +1,4 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { readPackageJsonSync } from '@map-colonies/read-pkg';
-
-export const CLI_NAME = readPackageJsonSync('../package.json').name ?? 'unknown_cli';
-
 export const TERMINAL_STREAM = process.stderr;
 
 export const CLI_BUILDER = Symbol('cliBuilder');
